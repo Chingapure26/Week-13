0bbff4eba61cc1cbb88d362565800891d1b02abc
rosario <rosarioayyala@gmail.com>
2021-05-02 02:04:20 -0300

    BQA-13:moncked

--- operations.mock.js
+++ operations.mock.js
@@ -1,2 +1,36 @@
 import 'jest';
 
+let isNumber = jest.fn().mockReturnValue(true);
+let isArray = jest.fn().mockReturnValue(true);
+let isString = jest.fn().mockReturnValue(true);
+let validateEmail = jest.fn().mockReturnValue(true);
+let sum = jest.fn().mockReturnValue(200);
+let multip = jest.fn().mockReturnValue(100);
+let division = jest.fn().mockReturnValue(100);
+let exponent = jest.fn().mockReturnValue(4);
+let isSupportedFigure = jest.fn().mockReturnValue(true);
+let arrayElementsAreObjectWithKey = jest.fn().mockReturnValue(true);
+let arrayIntersection = jest.fn().mockReturnValue(['Music']);
+let getEvenNumbersFromArray = jest.fn().mockReturnValue('[8, 10, 12]');
+let getOddNumbersFromArray = jest.fn().mockReturnValue('[5, 7, 9]');
+
+const funcMock = jest.mock('./basicOperations.js', () => {
+    return {
+        isNumber,
+        isArray,
+        isString,
+        validateEmail,
+        sum,
+        multip,
+        division,
+        exponent,
+        isSupportedFigure,
+        arrayElementsAreObjectWithKey,
+        arrayIntersection,
+        getEvenNumbersFromArray,
+        getOddNumbersFromArray
+    }
+});
+
+export default funcMock;
+
