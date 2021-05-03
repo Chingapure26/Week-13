9c2bfe96c6317c0e00c62d07b4135722283aa09c
rosario <rosarioayyala@gmail.com>
2021-05-03 18:50:36 -0300

    BQA:13:final mock test

--- Integrated and moncked Test/mockedcomplexOperations.test.js
+++ Integrated and moncked Test/mockedcomplexOperations.test.js
@@ -6,15 +6,6 @@ describe('ComplexOperation - Unit Tests', () => {
     it('Correct email', () => {
       expect(complexOperations.checkEmail('rosarioayyala@gmail.com')).toStrictEqual('The email is valid'); 
     });
-
-    it('Must be an string ', () => {
-      expect(complexOperations.checkEmail(1234)).toStrictEqual('The email should be an string');
-    });
-
-    it('Must be a valid', () => {
-      expect(complexOperations.checkEmail('Rosario Ayala')).toStrictEqual('The email is invalid');
-    });
-
   });
 
   describe('CalculateArea', () => {
@@ -31,15 +22,7 @@ describe('ComplexOperation - Unit Tests', () => {
     });
 
     it('Circle test for calculateArea', () => {
-      expect(complexOperations.calculateArea('circle')).toBeCloseTo(12.56);
-    });
-
-    it('Must be suported figure', () => {
-      expect(complexOperations.calculateArea('pyramid')).toBe(`${'pyramid'} is not supported`);
-    });
-
-    it('Must be a number', () => {
-      expect(complexOperations.calculateArea('circle','calculate','area')).toBe('number1 and number2 should be numbers');
+      expect(complexOperations.calculateArea('circle')).toBeCloseTo(12.566370614359172);
     });
   });
 
@@ -50,10 +33,6 @@ describe('ComplexOperation - Unit Tests', () => {
 
     it('Sum less than', () => {
       expect(complexOperations.sumGratherThan(0, 0, 500)).toBe(`${200} is less or equal than ${500}`);
-    });
-
-    it('Must be a number', () => {
-      expect(complexOperations.sumGratherThan('one', 'six', 'Hi')).toBe('The params should be numbers');
     });
   });
 
@@ -86,39 +65,11 @@ describe('ComplexOperation - Unit Tests', () => {
       expect(complexOperations.sortArrayOfObjectsByKey(paramArray, 'age')).toEqual(resultArray);  
     });
 
-    it('First element must be an array', () => {
-      expect(complexOperations.sortArrayOfObjectsByKey('param', 'age')).toStrictEqual('The first param should be an array');  
-    });
-
-    it('Second element must be an string', () => {
-      let resultArray = [ 
-        { age: 28, name: 'Rosario' }, 
-        { age: 32, name: 'Adrian' } 
-      ];
-
-      let paramArray = [
-        { age: 32, name: 'Adrian'},
-        { age: 28, name: 'Rosario'}
-      ];
-      
-      expect(complexOperations.sortArrayOfObjectsByKey(paramArray,resultArray)).toBe('The second param should be an string');
-    });
-
-
   });
 
   describe('numberOfOddAndEvenNumbers', () => {
     it('Calculate number', () => {
       expect(complexOperations.numberOfOddAndEvenNumbers([1,2, 3, 4])).toEqual({"even": 11, "odd": 9});
     });
-
-    it('Param must be an array', () => {
-      expect(complexOperations.numberOfOddAndEvenNumbers('prueba')).toBe('The param should be an array');
-    });
-
-    it('Array must be a number', () => {
-      expect(complexOperations.numberOfOddAndEvenNumbers(['prueba1', 'prueba2'])).toBe(`The array should have only numbers`);
-    });
-    
   });
 });
--- Integrated and moncked Test/operations.mock.js
+++ Integrated and moncked Test/operations.mock.js
@@ -10,6 +10,7 @@ let division = jest.fn().mockReturnValue(100);
 let exponent = jest.fn().mockReturnValue(4);
 let isSupportedFigure = jest.fn().mockReturnValue(true);
 let arrayElementsAreObjectWithKey = jest.fn().mockReturnValue(true);
+let sortArrayByKey = jest.fn().mockReturnValue([{ age: 28, name: 'Rosario' },{ age: 32, name:'Adrian'}]);
 let arrayIntersection = jest.fn().mockReturnValue(['Music']);
 let getEvenNumbersFromArray = jest.fn().mockReturnValue('[8, 10, 12]');
 let getOddNumbersFromArray = jest.fn().mockReturnValue('[5, 7, 9]');
@@ -26,6 +27,7 @@ const funcMock = jest.mock('./basicOperations.js', () => {
         exponent,
         isSupportedFigure,
         arrayElementsAreObjectWithKey,
+        sortArrayByKey,
         arrayIntersection,
         getEvenNumbersFromArray,
         getOddNumbersFromArray
